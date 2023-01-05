9afb3ffbb433282c637a3e0f280d616f6e1dd92c
Jagodyanocka <jagoda.kosewska@gmail.com>
2023-01-05 19:09:06 +0100

    add yml

---
+++ src/main/java/javangblog/springangularblog/SpringAngularBlogApplication.java
@@ -0,0 +1,13 @@
+package javangblog.springangularblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javangblog/springangularblog/SpringAngularBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package javangblog.springangularblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
